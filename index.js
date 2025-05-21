24c454b2766e2076ba29e17d11db57f0f98cc7e7
rzrgames <razirgames@gmail.com>
2025-05-21 19:35:52 +0000

    commit

--- index.js
+++ index.js
@@ -1,43 +1,150 @@
-
-
+#!/usr/bin/env node
 
 const http = require('http');
 const url = require('url');
 const ytdl = require('@distube/ytdl-core');
+const ffmpegPath = require('ffmpeg-static');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+const path = require('path');
 
-// Paste your raw cookies here (string format like "SID=...; HSID=...")
-//const cookies = 'YOUR_COOKIES_HERE';
-const userAgent = 'Mozilla/5.0 (Linux; Android 10; Mobile) AppleWebKit/537.36 Chrome/121 Safari/537.36';
-const cookies = `PREF=f6=40000000&tz=Asia.Jakarta&f7=100; GPS=1; __Secure-1PSIDTS=sidts-CjIBjplskJJ4ohq-ClEsZgDETLE50JqK03B_GJptTqz8PIip91IUs9IZPJ70DdtTpnSQzxAA; __Secure-3PSIDTS=sidts-CjIBjplskJJ4ohq-ClEsZgDETLE50JqK03B_GJptTqz8PIip91IUs9IZPJ70DdtTpnSQzxAA; HSID=AQTtmzbZaruNQ0XHb; SSID=AajBBOEZzGwFNsXMM; APISID=GLWC_qG1Uj2GsieB/AT56A5U_Rt7sMzXmk; SAPISID=HwScfpCooVn7d4uN/AF8kd6sPqPC3Klkq0; __Secure-1PAPISID=HwScfpCooVn7d4uN/AF8kd6sPqPC3Klkq0; __Secure-3PAPISID=HwScfpCooVn7d4uN/AF8kd6sPqPC3Klkq0; SID=g.a000xAgm7GQNXHtlkKVJs9yxy6YvrTAIQXGmhWQPxmCmauZoqZahSvbnTuW6PfRrXgkbCCIpTgACgYKAcESAQASFQHGX2MiDaQxpepGJWHe2q0NDFYzbBoVAUF8yKprFLUfmpEXMM9Qp_46Wy7t0076; __Secure-1PSID=g.a000xAgm7GQNXHtlkKVJs9yxy6YvrTAIQXGmhWQPxmCmauZoqZahiRu6rhAGJOdQb-YgSpD3awACgYKAX4SAQASFQHGX2Mi3iFu3qXnx6bt-G-6NaX8_hoVAUF8yKpPt7tezsIKvU_TslgYMQZW0076; __Secure-3PSID=g.a000xAgm7GQNXHtlkKVJs9yxy6YvrTAIQXGmhWQPxmCmauZoqZahXvhEZj5UbWTPV8s70j1ihwACgYKARESAQASFQHGX2Mi3PzlJxWw_icleAjZqv1VCRoVAUF8yKrC-Afuxb8tu3KaWRwKcNNM0076; LOGIN_INFO=AFmmF2swRQIgXqdshiq-NLatUBZxeOOk0gzJQH_cvvD1nNUnWbS6li8CIQCvhDMG8bcJVafulMj0_-5vKSrTW060cm7Wq4WAjyO1Jg:QUQ3MjNmeGJyNXJBcjBtUzhjcmNJU01CZkdFM3J6ejVaek1nV05hcW1nekhWMjdCS2V1TDZUZ1hWVXBRSTRNdUlMUW5lbGFYdHFlSE0zN0VRVnlJQm1tMkFHbnBqb3ZUVUozMG9oY0pXNlhjOG12UU9CUERlWUVmRXlDVkdGVko2QzRVVlUxSjM5ZHpObWtrU3N5bHQwNjNjYnc1aDVoRXRR; SIDCC=AKEyXzVMcTD2yarFFS7eG5cfD6IYcrjvYBpcV7DvDzwb2mEYIBar7agaf0TgTeqUlz_ukOg97w; __Secure-1PSIDCC=AKEyXzXPN7CTZt2CokARdk-w6ogRuXeLdWqXBRSWXXzWWLeoGEeMQRotEyLKmzHY8DMeywIA3w; __Secure-3PSIDCC=AKEyXzVeJFWV-9MLiFaXVfk0tiqvpMSKtEbNx573YUdI-ys7LEs9rrw7rsxnmJaC7DGZ_J8b1A`;
+ffmpeg.setFfmpegPath(ffmpegPath);
 
-const PORT = 3000;
+const formatsList = [
+  { name: 'MP3 - 320kbps', value: 'mp3_320' },
+  { name: 'MP3 - 256kbps', value: 'mp3_256' },
+  { name: 'MP3 - 128kbps', value: 'mp3_128' },
+  { name: 'MP4 - 1080p', value: '1080p' },
+  { name: 'MP4 - 720p', value: '720p' },
+  { name: 'MP4 - 360p', value: '360p' },
+];
+
+const PORT = 8080;
+
+function sanitizeTitle(title) {
+  return title.replace(/[\/\\?%*:|"<>]/g, '-');
+}
+
+async function handleInfo(req, res, videoUrl) {
+  try {
+    const info = await ytdl.getInfo(videoUrl);
+    const title = sanitizeTitle(info.videoDetails.title);
+    const audioFormats = info.formats.filter(f => f.hasAudio && !f.hasVideo && f.audioBitrate);
+    const videoFormats = info.formats.filter(f => f.hasAudio && f.hasVideo && f.container === 'mp4');
+
+    const response = {
+      title,
+      formats: {
+        mp3: {},
+        mp4: {}
+      }
+    };
+
+    for (const fmt of formatsList) {
+      if (fmt.value.startsWith('mp3')) {
+        const target = parseInt(fmt.value.split('_')[1], 10);
+        let chosen = audioFormats.find(f => f.audioBitrate <= target);
+        if (!chosen) chosen = audioFormats[audioFormats.length - 1];
+
+        response.formats.mp3[fmt.value] = {
+          approx: `${chosen.audioBitrate} kbps`,
+          url: chosen.url
+        };
+      } else {
+        const height = parseInt(fmt.value);
+        let chosen = videoFormats.find(f => f.height && f.height <= height);
+        if (!chosen) chosen = videoFormats[videoFormats.length - 1];
+
+        response.formats.mp4[fmt.value] = {
+          approx: `${chosen.height}p`,
+          url: chosen.url
+        };
+      }
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(response, null, 2));
+
+  } catch (err) {
+    res.writeHead(500);
+    res.end('Error fetching video info: ' + err.message);
+  }
+}
+
+async function handleDownload(req, res, videoUrl, format) {
+  try {
+    const info = await ytdl.getInfo(videoUrl);
+    const title = sanitizeTitle(info.videoDetails.title);
+
+    if (format.startsWith('mp3')) {
+      const targetBitrate = parseInt(format.split('_')[1], 10);
+      const audioFormats = info.formats
+        .filter(f => f.hasAudio && !f.hasVideo && f.audioBitrate)
+        .sort((a, b) => b.audioBitrate - a.audioBitrate);
+
+      let chosen = audioFormats.find(f => f.audioBitrate <= targetBitrate);
+      if (!chosen) chosen = audioFormats[0];
+
+      if (chosen.audioBitrate < targetBitrate) {
+        // Re-encode using ffmpeg
+        const output = `${title}_${targetBitrate}kbps.mp3`;
+        res.writeHead(200, {
+          'Content-Type': 'audio/mpeg',
+          'Content-Disposition': `attachment; filename="${output}"`
+        });
+
+        const stream = ytdl(videoUrl, { format: chosen });
+
+        ffmpeg(stream)
+          .audioBitrate(targetBitrate)
+          .format('mp3')
+          .pipe(res)
+          .on('error', err => {
+            res.writeHead(500);
+            res.end('FFmpeg error: ' + err.message);
+          });
+
+      } else {
+        // Serve direct stream
+        res.writeHead(302, { Location: chosen.url });
+        res.end();
+      }
+
+    } else if (format.endsWith('p')) {
+      const height = parseInt(format);
+      const videoFormats = info.formats
+        .filter(f => f.hasVideo && f.hasAudio && f.container === 'mp4' && f.height)
+        .sort((a, b) => b.height - a.height);
+
+      let chosen = videoFormats.find(f => f.height <= height);
+      if (!chosen) chosen = videoFormats[0];
+
+      res.writeHead(302, { Location: chosen.url });
+      res.end();
+    } else {
+      res.writeHead(400);
+      res.end('Invalid format');
+    }
+  } catch (err) {
+    res.writeHead(500);
+    res.end('Download error: ' + err.message);
+  }
+}
 
 http.createServer(async (req, res) => {
   const parsed = url.parse(req.url, true);
-  const videoUrl = parsed.query.url;
+  const { pathname, query } = parsed;
 
-  if (parsed.pathname === '/info' && videoUrl) {
-    try {
-      const info = await ytdl.getInfo(videoUrl, {
-        requestOptions: {
-          headers: {
-            cookie: cookies,
-            'User-Agent': userAgent,
-          }
-        }
-      });
+  if (pathname === '/info' && query.url) {
+    return handleInfo(req, res, query.url);
+  }
 
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(info.videoDetails, null, 2));
-    } catch (err) {
-      res.writeHead(500);
-      res.end('Error: ' + err.message);
-    }
-    return;
+  if (pathname === '/download' && query.url && query.format) {
+    return handleDownload(req, res, query.url, query.format);
   }
 
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Use /info?url=...');
+  res.end('Use /info?url=... or /download?url=...&format=...');
 }).listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
